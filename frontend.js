42c4b8250984d411baa7b8e5432f694927319cef
Shannu-123 <shanmukha.voruganti@gmail.com>
2019-01-15 15:38:45 +0530

    opacity

--- frontend.js
+++ frontend.js
@@ -2,7 +2,6 @@ var doc=$(document);
 var window=$(window);
 doc.ready(function(){
     doc.scroll(function(){
-    console.log(doc.scrollTop());
     if(doc.scrollTop()>85)
     {
         $('.nav-bar').addClass('scroll');
